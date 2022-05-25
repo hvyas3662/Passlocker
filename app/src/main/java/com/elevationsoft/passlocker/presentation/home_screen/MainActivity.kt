5449815fa12a2f788eedb628b85d7032b1181ce5
harshal <hitesh.vyas3662@gmail.com>
2022-05-25 18:32:37 +0530

    Theme related fix

--- app/src/main/java/com/elevationsoft/passlocker/presentation/home_screen/MainActivity.kt
+++ app/src/main/java/com/elevationsoft/passlocker/presentation/home_screen/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.elevationsoft.passlocker.presentation.home_screen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.elevationsoft.passlocker.databinding.ActivityMainBinding
+import com.elevationsoft.passlocker.presentation.add_category.AddCategoryActivity
+import com.elevationsoft.passlocker.presentation.add_credentials.AddCredentialsActivity
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var selectedTabIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        selectedTabIndex = binding.ctvBottomTabBar.getSelectedTabIndex()
         binding.ctvBottomTabBar.setOnTabChangedCallBack { tabIndex ->
             Timber.d("selected tab: $tabIndex")
+            selectedTabIndex = tabIndex
         }
 
-
+        binding.ivAdd.setOnClickListener {
+            if (selectedTabIndex == 0) {
+                startActivity(Intent(this@MainActivity, AddCredentialsActivity::class.java))
+            } else {
+                startActivity(Intent(this@MainActivity, AddCategoryActivity::class.java))
+            }
+        }
     }
 }
